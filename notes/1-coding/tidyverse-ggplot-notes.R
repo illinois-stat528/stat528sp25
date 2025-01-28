5cbb6e3d6af3ef0ea7b3071158d9e3e58d89a659
Daniel Eck <deck1388@gmail.com>
2025-01-28 14:07:33 -0600

    updated tidyverse/ggplot R script

--- notes/1-coding/tidyverse-ggplot-notes.R
+++ notes/1-coding/tidyverse-ggplot-notes.R
@@ -30,7 +30,7 @@ People = as_tibble(People)
 select(Batting, HR)
 
 ### filter 
-filter(Batting, yearID == 2013)x
+filter(Batting, yearID == 2013)
 
 ### arrange 
 arrange(Batting, HR)
@@ -59,13 +59,29 @@ Batting %>%
   summarise(meanHR = mean(HR)) %>% 
   arrange(desc(meanHR))
 
-
-## Week 2: Tuesday
+Batting %>% 
+  group_by(playerID, yearID) %>% 
+  summarise(HR = sum(HR)) %>% 
+  group_by(playerID) %>% 
+  summarise(meanHR = mean(HR)) %>% 
+  arrange(desc(meanHR))
 
 ### left_join 
 
 #### player names
+foo = Batting %>% 
+  group_by(playerID, yearID) %>% 
+  summarise(HR = sum(HR)) %>% 
+  group_by(playerID) %>% 
+  summarise(meanHR = mean(HR)) %>% 
+  arrange(desc(meanHR))
 
+bar = People %>% 
+  mutate(name = paste(nameFirst, nameLast, sep = " ")) %>% 
+  select(playerID, name)
+
+dat_avg_HR = foo %>% 
+  left_join(bar, by = "playerID")
 
 ## readr
 
@@ -88,11 +104,24 @@ season_eHR
 
 ### career home runs (arranged from high to low)
 ### add final year and career at bats
+career_HR = Batting %>% 
+  group_by(playerID) %>% 
+  summarise(HR = sum(HR), AB = sum(AB), finalYear = max(yearID)) %>% 
+  arrange(desc(HR))
+  
+
 ### add player names
-career_HR 
+career_HR = Batting %>% 
+  group_by(playerID) %>% 
+  summarise(HR = sum(HR), AB = sum(AB), finalYear = max(yearID)) %>% 
+  arrange(desc(HR)) 
+
 
 ### career home runs and career era-adjusted home runs (left_join)
-career_eHR
+career_eHR  = season_eHR %>% 
+  group_by(playerID) %>% 
+  summarise(eHR = sum(HR), eAB = sum(AB), eFinalYear = max(year)) %>% 
+  arrange(desc(eHR)) 
 
 datHR = career_HR %>% 
   left_join(career_eHR, by = "playerID")
@@ -105,11 +134,55 @@ datHR
 ### add geometry
 ### add a theme
 ### add a title and axes labels
+ggplot(career_HR) + 
+  aes(x = finalYear, y = HR) + 
+  geom_point() + 
+  geom_smooth() +
+  theme_minimal() + 
+  labs(title = "career home runs over time", 
+       x = "final year",
+       y = " home runs")
 
+ggplot(career_eHR) + 
+  aes(x = eFinalYear, y = eHR) + 
+  geom_point() + 
+  geom_smooth() +
+  theme_minimal() + 
+  labs(title = "Career era-adjusted home runs over time", 
+       x = "final year",
+       y = " home runs")
 
 ### career home runs vs era-adjusted home runs by final year (min 1000 AB)
+career_HR %>% 
+  filter(AB >= 1000) %>% 
+  ggplot() + 
+  aes(x = finalYear, y = HR) + 
+  geom_point() + 
+  geom_smooth() +
+  theme_minimal() + 
+  labs(title = "Career home runs over time", 
+       subtitle = "minimum 1000 at bats",
+       x = "final year",
+       y = " home runs")
+
+career_eHR %>% 
+  filter(eAB >= 1000) %>% 
+  ggplot() + 
+  aes(x = eFinalYear, y = eHR) + 
+  geom_point() + 
+  geom_smooth() +
+  theme_minimal() + 
+  labs(title = "Career era-adjusted home runs over time", 
+       subtitle = "minimum 1000 at bats",
+       x = "final year",
+       y = " home runs")
 
 
+career_HR = career_HR %>% 
+  filter(AB >= 1000) 
+
+career_eHR = career_eHR %>% 
+  filter(eAB >= 1000)
 
 #### get common playerIDs in these data sets
 ?intersect
@@ -118,20 +191,27 @@ playerIDs = intersect(career_HR$playerID,
 
 ### continue with comparison of career home runs vs era-adjusted home runs
 
-
-
 #### make larger data set with original and era-adjusted 
 #### HR counts
 ?bind_rows
-datHR_binded = 
-#mutate(id = ifelse(id == 1, "original", "era-adjusted"))
+datHR_binded = bind_rows(career_HR %>% 
+                           filter(playerID %in% playerIDs), 
+                         career_eHR %>% 
+                           filter(playerID %in% playerIDs) %>% 
+                           rename(HR = eHR, AB = eAB, finalYear = eFinalYear), 
+                         .id = "id") %>% 
+  mutate(id = ifelse(id == 1, "observed", "era-adjusted"))
 
-
-#### pivot
-  
   
 #### separate trend lines (col in aes)
-
+ggplot(datHR_binded) + 
+  aes(x = finalYear, y = HR, col = id) +
+  geom_smooth() + 
+  theme_minimal() + 
+  labs(title = "Smoothed career home runs over time", 
+       subtitle = "minimum 1000 at bats",
+       x = "final year",
+       y = " home runs")
 
 #### better plot
 ggplot(datHR_binded) + 
@@ -150,13 +230,39 @@ ggplot(datHR_binded) +
   theme_minimal()
 
 #### facet_wrap
-ggplot(datHR) +
-  aes(x = finalYear, y = HR, col = id) 
+ggplot(datHR_binded) +
+  aes(x = finalYear, y = HR, col = id) + 
+  geom_point(col = "black") + 
+  geom_smooth() + 
+  facet_wrap(~id) +
+  labs(title = "Smoothed home run count by a player's final year (min 1000 ABs)", 
+       subtitle = "Comparison of raw and era-adjusted counts") + 
+  theme_minimal()
 
 
 
 ### single season HR by position 1982-1993 (minimum 100 AB)
+Fielding_small = Fielding %>% 
+  select(playerID, yearID, POS, InnOuts) %>% 
+  group_by(playerID, yearID) %>% 
+  filter(InnOuts == max(InnOuts)) %>% 
+  filter(POS != "P", !is.na(POS)) %>% 
+  rename("primaryPOS" = POS) %>% 
+  select(playerID, yearID, primaryPOS)
 
+Batting %>% 
+  filter(yearID >= 1982, yearID <= 1993, AB >= 100) %>% 
+  select(playerID, yearID, HR) %>% 
+  left_join(Fielding_small) %>% 
+  filter(!is.na(primaryPOS)) %>% 
+  ggplot() + 
+  aes(x = HR, y = primaryPOS, fill = primaryPOS) + 
+  geom_density_ridges() + 
+  theme_minimal() + 
+  labs(title = "Distribution of seasonal home run counts", 
+       subtitle = "by primary position", 
+       x = "home runs", 
+       y = "primary position")
 
 #### Notes: 
 ## https://daviddalpiaz.org/posts/moneyball-in-r/

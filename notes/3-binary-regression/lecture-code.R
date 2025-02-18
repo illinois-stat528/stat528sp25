cb89c50ed84ec48a83b56ab15b5afe38b413f2a5
Daniel Eck <deck1388@gmail.com>
2025-02-18 15:54:52 -0600

    added lecture code

--- notes/3-binary-regression/lecture-code.R
+++ notes/3-binary-regression/lecture-code.R
@@ -22,14 +22,47 @@ CCSO %>%
 # variables
 
 ## biological sex
+CCSO %>% 
+  filter(crimeCode == "OTHER TRAFFIC OFFENSES") %>% 
+  mutate(atleastone = ifelse(daysInJail >= 1,1,0)) %>% 
+  group_by(sex) %>% 
+  summarise(n = n(), mean(atleastone))
 
 ## race
+CCSO %>% 
+  filter(crimeCode == "OTHER TRAFFIC OFFENSES") %>% 
+  mutate(atleastone = ifelse(daysInJail >= 1,1,0)) %>% 
+  group_by(race) %>% 
+  summarise(n = n(), mean(atleastone))
 
-## age at arrest
+## arrestAge
+CCSO %>% 
+  filter(crimeCode == "OTHER TRAFFIC OFFENSES") %>% 
+  mutate(atleastone = ifelse(daysInJail >= 1,1,0)) %>% 
+  filter(arrestAge <= 90) %>% 
+  ggplot() + 
+  aes(x = arrestAge, y = atleastone) + 
+  geom_smooth(method = "lm") +
+  geom_point() +
+  theme_minimal()
 
 ## released reason
+CCSO %>% 
+  filter(crimeCode == "OTHER TRAFFIC OFFENSES") %>% 
+  mutate(atleastone = ifelse(daysInJail >= 1,1,0)) %>% 
+  group_by(releasedReason) %>% 
+  summarise(n = n(), mean(atleastone)) %>% 
+  as.data.frame()
 
 ## transfers or sentence
+CCSO %>% 
+  filter(crimeCode == "OTHER TRAFFIC OFFENSES") %>% 
+  mutate(atleastone = ifelse(daysInJail >= 1,1,0)) %>% 
+  filter(grepl("Transfer|Sentence", releasedReason)) %>% 
+  group_by(race) %>% 
+  summarise(n = n(), mean(atleastone))
+
+
 
 # data transformation (simple demographic variables)
 CCSO_small = CCSO %>% 
@@ -44,9 +77,13 @@ head(CCSO_small)
 dim(CCSO_small)
 
 # fit a basic logistic regression model (race, sex, arrestAge)
+m1 = glm(atleastone ~ -1 + race + sex + arrestAge, data = CCSO_small, 
+         family = "binomial")
 ?glm
 
 ## x = TRUE returns a model matrix
+m1 = glm(atleastone ~ -1 + race + sex + arrestAge, data = CCSO_small, 
+         family = "binomial", x = TRUE)
 M = m1$x
 head(M)
 
@@ -57,26 +94,40 @@ class(summary(m1))
 summary(m1)
 
 ## baseline propensities for racial subgroups
+betahat = coef(m1)
+1/(1 + exp(-betahat[1:4]))
 
 ## check standard errors against vcov (all.equal)
 ?vcov
-
+sqrt(diag(vcov(m1)))
+all.equal(summary(m1)$coef[, 2], sqrt(diag(vcov(m1))))
 ?all.equal
 sqrt(.Machine$double.eps)
 ?.Machine
 
 ## compare with saturated model 
+summary(m1)
+pchisq(m1$deviance, m1$df.residual, lower = FALSE)
 
 ## Why does Deviance = -2*log likelihood?
+-2 * logLik(m1)
+phat = predict(m1, type = "response")
+y = CCSO_small$atleastone
+-2 * sum(y*log(phat) + (1-y)*log(1-phat))
 
 ## compare with intercept only model (what is this?)
-
+pintercept = CCSO_small %>% 
+  pull(atleastone) %>% 
+  mean()
 
 ### deviance test calculated by hand
 logLik_intercept = sum(y*log(pintercept) + (1-y)*log(1-pintercept))
 logLik_intercept
 deviance_intercept = -2*logLik_intercept
 deviance_intercept
+
+summary(m1)
+pchisq(deviance_intercept - m1$deviance, 5, lower = FALSE)
 
 
 ### using anova
@@ -85,13 +136,24 @@ mintercept = glm(atleastone ~ 1, data = CCSO_small,
 anova(mintercept, m1, test = "LRT")
 
 ### estimated probabilities the same as the mean
+unique(predict(mintercept, type = "response"))
 
 
 ## investigate the influence of biological sex
-
+m2 = glm(atleastone ~ race + arrestAge, data = CCSO_small, 
+         family = "binomial")
+summary(m2)
+anova(m2, m1, test = "LRT")
+AIC(mintercept)
+AIC(m1)
+AIC(m2)
 
 # careful with interpretation
 ## fit model with intercept
+summary(m1)
+m2 = glm(atleastone ~ race + sex + arrestAge, data = CCSO_small, 
+         family = "binomial")
+cbind(betahat, coef(m2))
 
 ## orthogonal transformation
 M2 = M %*% eigen(var(M))$vec
@@ -99,8 +161,13 @@ m3 = glm(CCSO_small$atleastone ~ -1 + M2, family = "binomial")
 cbind(betahat, coef(m2), coef(m3))
 
 ### same log likelihood
+logLik(m1)
+logLik(m2)
+logLik(m3)
 
 ### same mean-value parameter estimates
+all.equal(predict(m1, type = "response"),
+predict(m3, type = "response"))
 
 # sub model canonical parameter vector (beta)
 betahat
@@ -125,6 +192,10 @@ tauhat
 ## observed = expected (full regular exponential family fact)
 cbind(tauhat, crossprod(M, y))
 
+
+###############
+### next time
+###############
 
 # Esarey and Pierce (2012) diagnostic plot
 library(heatmapFit)
